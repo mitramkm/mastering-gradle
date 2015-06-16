983c7f35df0bec51cc6d40fffeffd1acba492aed
unknown <3p-maheshwaria@HYDLT-5TNVTQ1.icepor.com>
2015-06-16 08:59:03 +0530

    Added QualityCheck

---
+++ Chapter7/QualityCheck/src/main/java/ch7/SampleTask.java
@@ -0,0 +1,8 @@
+package ch7;
+
+class SampleTask  {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+}
--- Chapter7/qualitycheck/src/main/java/ch7/SampleTask.java
+++
@@ -1,8 +0,0 @@
-package ch7;
-
-class SampleTask  {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-
-	}
-}
