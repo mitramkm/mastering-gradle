d8042a93f766197c61c98fe8a4ebbdcf45cf0f23
unknown <3p-maheshwaria@HYDLT-5TNVTQ1.icepor.com>
2015-06-17 12:51:37 +0530

    renamed com to ch6

---
+++ Chapter6/testUsingJunitParallel/src/test/java/ch6/login/LoginTest2.java
@@ -0,0 +1,34 @@
+package ch6.login;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class LoginTest2 {
+	private String userName="Gradle";
+	 
+	@BeforeClass
+    	public static void oneTimeSetUp() {
+        	// one-time initialization code   
+    		System.out.println("@BeforeClass - oneTimeSetUp");
+    	}
+ 
+    	@AfterClass
+    	public static void oneTimeTearDown() {
+        	// one-time cleanup code
+    		System.out.println("@AfterClass - oneTimeTearDown");
+    	}
+ 
+	@Test
+    	public void testUserName() {
+        	System.out.println("LoginTest: Testing Login ");
+		assertTrue(userName.equals("Gradle"));
+    	}
+ 
+	@Test
+    	public void testUserNameNotNull() {
+        	System.out.println("LoginTest: Testing Login 2");
+		assertTrue(userName != null);
+    	}
+    
+}
