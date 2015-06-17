9a84247f5003bcb8fd46eb5919e5f3c2b56603c1
unknown <3p-maheshwaria@HYDLT-5TNVTQ1.icepor.com>
2015-06-17 13:44:52 +0530

    renamed com to ch6

---
+++ Chapter6/UserManagement_taskDep/login/src/main/java/ch6/login/LoginService.java
@@ -0,0 +1,7 @@
+package ch6.login;
+
+class LoginService {
+	public static void main(String[] args) {
+		System.out.println("This is login service");
+	}
+}
--- Chapter6/UserManagement_taskDep/login/src/main/java/com/user/login/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.user.login;
-
-class LoginService {
-	public static void main(String[] args) {
-		System.out.println("This is login service");
-	}
-}
